8c6c4edcb2acc1e33ea9c5e079d8bf5adf2e0de7
ApacheTech Solutions <p_matthews@live.co.uk>
2025-03-08 23:58:48 +0000

    Synchronising changes across repositories

--- src/Shuffle/Properties/AssemblyAttribution.cs
+++ src/Shuffle/Properties/AssemblyAttribution.cs
@@ -2,14 +2,13 @@
 
 using System.Reflection;
 
-[assembly: ModDependency("game", "1.20.1")]
 [assembly: AssemblyMetadata("Disclaimer", "Experimental release. Not for public use.")]
 [assembly: ModInfo(
     "Shuffle",
     "shuffle",
     Description = "Shuffle allows players to place blocks randomly from their hotbar.",
     Side = "Client",
-    Version = "1.0.0",
+    Version = "1.0.1",
     NetworkVersion = "1.0.0",
     Website = "https://apachegaming.net",
     Contributors = ["Apache"],
